512f733090038c0c7643b70c98b796de430ec37e
Grimoire <quocanhnguyen1532@gmail.com>
2022-02-08 15:18:16 +0700

    Complete home page

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -12,27 +12,6 @@
     </div>
     <div class="container">
         <div class="row karl-new-arrivals">
-            @foreach($product as $item)
-            <!-- Single gallery Item Start -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.2s">
-                <!-- Product Image -->
-                <div class="product-img">
-                <img style="height:200px" src="{{$item['thumbnail']}}" alt="{{$item['thumbnail']}}"></td>
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-                    </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">{{$item['price']}} VND</h4>
-                    <p>{{$item['name']}}</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">Thêm vào giỏ</a>
-                </div>
-            </div>
-            @endforeach
-
-
         </div>
     </div>
 
